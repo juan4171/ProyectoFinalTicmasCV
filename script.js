61d5f10f4b74544afee66a1cfc066055ea4124ba
Juan M <juan82montalivet@gmail.com>
2023-01-14 03:46:21 -0300

    modificaciones varias en las dos primeras secciones

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ fetch(url)
         console.log(json)
         let data = json.results[0];
         let nombre = (data.name.first + " " + data.name.last);
-        document.getElementById("introduccion").innerHTML = ("Hola, mi nombre es " + nombre + " y esta es mi presentacion:");
-        document.getElementById("titulo").innerHTML = (nombre + "<br>Curriculum vitae") ;
+        document.getElementById("introduccion").innerHTML = ("Hola, mi nombre es " + nombre + " y este es mi Curriculum vitae:");
+        document.getElementById("titulo").innerHTML = (nombre + "<br> Curriculum");
         document.getElementById("cara").src=data.picture.large;
-        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de la carrera Licenciatura en informatica en la facultad de informática de la Universidad Nacional de " + data.location.city + ", " + data.location.state + ". Actualmente estoy interesado en tener mi primera experiencia laboral como programador/developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
+        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de programacion en la facultad de informática de la Universidad Nacional de " + data.location.city + ", " + data.location.state + ". Actualmente estoy interesado en tener mi primera experiencia laboral como programador/developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
         console.log(document.getElementById("cara"))
     });
 ;
