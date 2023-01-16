3d476fad150d2a61ab1b4b0ae97c78d131d3c175
Juan M <juan82montalivet@gmail.com>
2023-01-16 02:16:52 -0300

    agrego section de proyectos

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 /* obtengo datos de una persona ficticia con la api de randomuser */
-let url = "https://randomuser.me/api/?inc=gender,name,email,phone,picture,phone,location,nat&nat=us&gender=male&noinfo&results=1"
+let url = "https://randomuser.me/api/?inc=gender,dob,name,email,phone,picture,phone,location,nat&nat=us&gender=male&noinfo&results=1"
 fetch(url)
     .then(Response => Response.json())
     .then(json => {
@@ -10,7 +10,7 @@ fetch(url)
         document.getElementById("introduccion").innerHTML = ("Hola, mi nombre es " + nombre + " y este es mi Curriculum vitae:");
         document.getElementById("titulo").innerHTML = (nombre + "<br> Curriculum");
         document.getElementById("cara").src=data.picture.large;
-        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de programacion en la facultad de informática de la Universidad Nacional de " + data.location.city + ", " + data.location.state + ". Actualmente estoy interesado en tener mi primera experiencia laboral como programador/developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
+        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de programacion en la facultad de informática de " + data.location.state + ". Actualmente a mis "+data.dob.age+" años de edad vivo en " + data.location.city + ", " + data.location.state + " y estoy interesado en tener nuevas experiencias laborales como developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
         console.log(document.getElementById("cara"))
     });
 ;
