4a7d78591be9e78f1a6ae9583e9934142de88785
Juan M <juan82montalivet@gmail.com>
2023-01-18 04:23:38 -0300

    agrego funcion a botones

--- script.js
+++ script.js
@@ -10,8 +10,29 @@ fetch(url)
         document.getElementById("introduccion").innerHTML = ("Hola, mi nombre es " + nombre + " y este es mi Curriculum vitae:");
         document.getElementById("titulo").innerHTML = (nombre + "<br> Curriculum");
         document.getElementById("cara").src=data.picture.large;
-        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de programacion en la facultad de informática de " + data.location.state + ". Actualmente a mis "+data.dob.age+" años de edad vivo en " + data.location.city + ", " + data.location.state + " y estoy interesado en tener nuevas experiencias laborales como developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
+        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de programacion en la universidad informática de " + data.location.state + ". Actualmente a mis "+data.dob.age+" años de edad vivo en " + data.location.city + ", " + data.location.state + " y estoy interesado en tener nuevas experiencias laborales como developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
         console.log(document.getElementById("cara"))
     });
 ;
+/* logica para los botones que muestran informacion en la section proyectos */
+document.getElementById("button1").addEventListener("click", function(){
+    var x = document.getElementById("parrafo1");
+    cambiarDisplay(x)
+});
+document.getElementById("button2").addEventListener("click", function(){
+    var x = document.getElementById("parrafo2");
+    cambiarDisplay(x)
+});
+document.getElementById("button3").addEventListener("click", function(){
+    var x = document.getElementById("parrafo3");
+    cambiarDisplay(x)
+});
+function cambiarDisplay (x){
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
 
