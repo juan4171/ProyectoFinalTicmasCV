0a132580511f4957bc6272bd23196157a663f7e8
Juan M <juan82montalivet@gmail.com>
2023-01-21 03:00:44 -0300

    agrego iconos y seccion de contacto

--- script.js
+++ script.js
@@ -4,28 +4,29 @@ let url = "https://randomuser.me/api/?inc=gender,dob,name,email,phone,picture,ph
 fetch(url)
     .then(Response => Response.json())
     .then(json => {
-        console.log(json)
+        //console.log(json)
         let data = json.results[0];
         let nombre = (data.name.first + " " + data.name.last);
-        document.getElementById("introduccion").innerHTML = ("Hola, mi nombre es " + nombre + " y este es mi Curriculum vitae:");
-        document.getElementById("titulo").innerHTML = (nombre + "<br> Curriculum");
-        document.getElementById("cara").src=data.picture.large;
-        document.getElementById("parrafo-introduccion").innerHTML= ("Soy un estudiante avanzado de programacion en la universidad informática de " + data.location.state + ". Actualmente a mis "+data.dob.age+" años de edad vivo en " + data.location.city + ", " + data.location.state + " y estoy interesado en tener nuevas experiencias laborales como developer para sumar nuevos conocimientos y afianzar los que ya tengo.");
-        console.log(document.getElementById("cara"))
+        // let nombre = (`${data.name.first} ${data.name.last}`);
+        let location = (`${data.location.city}, ${data.location.state}`);
+        document.getElementById("introduccion").innerHTML = (`Hola, mi nombre es ${nombre} y este es mi Curriculum vitae:`);
+        document.getElementById("titulo").innerHTML = (`${nombre}<br>Curriculum`);
+        document.getElementById("cara").src = data.picture.large;
+        document.getElementById("parrafo-introduccion").innerHTML= (`Soy un estudiante avanzado de programacion en la universidad informática de ${data.location.state}. Actualmente a mis ${data.dob.age} años de edad vivo en ${location} y estoy interesado en tener nuevas experiencias laborales como developer para sumar nuevos conocimientos y afianzar los que ya tengo.`);
+        document.getElementById("mail").innerHTML += (data.email);
+        document.getElementById("numero").innerHTML += (data.phone);
+        document.getElementById("ubicacion").innerHTML += (location);
     });
 ;
 /* logica para los botones que muestran informacion en la section proyectos */
 document.getElementById("button1").addEventListener("click", function(){
-    var x = document.getElementById("parrafo1");
-    cambiarDisplay(x)
+    cambiarDisplay(document.getElementById("parrafo1"));
 });
 document.getElementById("button2").addEventListener("click", function(){
-    var x = document.getElementById("parrafo2");
-    cambiarDisplay(x)
+    cambiarDisplay(document.getElementById("parrafo2"));
 });
 document.getElementById("button3").addEventListener("click", function(){
-    var x = document.getElementById("parrafo3");
-    cambiarDisplay(x)
+    cambiarDisplay(document.getElementById("parrafo3"));
 });
 function cambiarDisplay (x){
     if (x.style.display === "block") {
